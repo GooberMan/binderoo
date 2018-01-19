2bfca5f813b4a22264e8acb5e61fb29cf686f268
Ethan Watson <gooberman@gmail.com>
2018-01-19 06:53:49 +0200

    Struct-of-arrays now has an implementation

--- binderoo_client/d/src/binderoo/util/structofarrays.d
+++ binderoo_client/d/src/binderoo/util/structofarrays.d
@@ -33,10 +33,19 @@ module binderoo.util.structofarrays;
 import binderoo.traits;
 //----------------------------------------------------------------------------
 
+// The SOA template will rewrite the provided type to a new struct, with each
+// member variable of the Descriptor represented by a dynamic array of the
+// same type.
+//----------------------------------------------------------------------------
+
 struct SOA( alias Descriptor )
 {
 	mixin( GenerateSOAImplementation!( Descriptor )() );
 }
+//----------------------------------------------------------------------------
+
+// Instead of writing a proper struct, you can also just plain provide variables
+// as a string, ie SOA!( "int foo; int bar;" )
 //----------------------------------------------------------------------------
 
 template SOA( string variables )
@@ -50,29 +59,158 @@ template SOA( string variables )
 }
 //----------------------------------------------------------------------------
 
+// This ByMember iterator functionality will iterate over your array by member
+// regardless of if it's a traditional array of structs, or one of our templated
+// struct of arrays defined by the SOA template. As such, if you want your code
+// to switch over one way or the other, using this ByMember iterator on all your
+// things will allow you to switch implementations at will.
+//
+// For things that are already arrays, we need to define a new iterator for it.
+//----------------------------------------------------------------------------
+
+auto ByMember( string Member, T )( ref T[] obj )
+{
+	static if( IsUserType!T )
+	{
+		mixin( "import " ~ ModuleName!( T ) ~ ";" );
+		mixin( "alias MemberType = typeof( T." ~ Member ~ " );" );
+	}
+
+	// Currently a full iterator that preserves original range.
+	// Probably worth making it non-preservational.
+	struct Iterator
+	{
+		private T[] range;
+
+		@property save() const							{ return Iterator( range, iFront, iBack ); }
+		@property length() const						{ return range.length; }
+		@property bool empty() const					{ return range.length == 0; }
+
+		void popFront()									{ ++iFront; }
+		@property ref MemberType front() const			{ mixin( "return range[ 0 ]." ~ Member ~ ";" ); }
+
+		void popBack()									{ --iBack; }
+		@property ref MemberType back() const			{ mixin( "return range[ 0 ]." ~ Member ~ ";" ); }
+
+		ref MemberType opIndex( size_t uIndex ) const	{ mixin( "return range[ uIndex ]." ~ Member ~ ";" ); }
+		size_t opDollar() const							{ return range.length; }
+		Iterator opSlice( size_t uBegin, size_t uEnd )	{ return Iterator( range[ uBegin .. uEnd ] ); }
+	}
+
+	return Iterator( obj );
+}
+//----------------------------------------------------------------------------
+
+// Since the SOA template already has elements represented by slices, we simply
+// return the member slice. Simple.
+//----------------------------------------------------------------------------
+
+auto ByMember( string Member, T : SOA!( U ), U )( ref T obj )
+{
+	mixin( "return obj." ~ Member ~ ";" );
+}
+//----------------------------------------------------------------------------
+
 // IMPLEMENTATION FOLLOWS
 //----------------------------------------------------------------------------
 
-private string GenerateSOAImplemenation( alias Descriptor )()
+private string GenerateSOAImplementation( alias Descriptor )()
 {
 	string generateIterator()
 	{
-		return "";
+		string[] strOutput;
+		strOutput ~= "private:";
+		strOutput ~= "import " ~ ModuleName!( Descriptor ) ~ ";";
+		strOutput ~= "alias StructType = " ~ ModuleLocalTypeName!( Descriptor ) ~ ";";
+		strOutput ~= "alias SOAType = typeof( this );";
+		strOutput ~= "struct Iterator";
+		strOutput ~= "{";
+		strOutput ~= "\tpackage SOAType* pBase = null;";
+		strOutput ~= "\tpackage size_t iIndex = size_t.max;";
+		strOutput ~= "";
+		foreach( iIndex, member; Descriptor.init.tupleof )
+		{
+			static if( IsUserType!( typeof( Descriptor.tupleof[ iIndex ] ) ) )
+			{
+				enum ThisMemberType = ModuleLocalTypeName!( typeof( Descriptor.tupleof[ iIndex ] ) );
+			}
+			else
+			{
+				enum ThisMemberType = typeof( Descriptor.tupleof[ iIndex ] ).stringof;
+			}
+			enum ThisMemberName = __traits( identifier, Descriptor.tupleof[ iIndex ] );
+			enum ThisMemberAccessor = "pBase." ~ ThisMemberName ~ "[ iIndex ]";
+
+			strOutput ~= "\t@property ref " ~ ThisMemberType ~ " " ~ ThisMemberName ~ "() { return " ~ ThisMemberAccessor ~ "; }";
+			strOutput ~= "\t@property ref " ~ ThisMemberType ~ " " ~ ThisMemberName ~ "( " ~ ThisMemberType ~ " val ) { " ~ ThisMemberAccessor ~ " = val; return " ~ ThisMemberAccessor ~ "; }";
+			strOutput ~= "";
+		}
+		strOutput ~= "}";
+
+		return strOutput.joinWith( "\t", "", "\n" );
+	}
+
+	string generateLengthProperties()
+	{
+		return "\t@property size_t length() const { return 0; }\n\t@property size_t length( size_t uNewLength ) { return uNewLength; }";
+	}
+
+	string generateOperators()
+	{
+		string[] strOutput;
+
+		strOutput ~= "Iterator opIndex( size_t uIndex ) { return Iterator( &this, uIndex ); }";
+
+		return strOutput.joinWith( "\t", "", "\n" );
 	}
 
 	string generateVariables()
 	{
-		string[] strOuput;
+		string[] strOutput;
 
 		foreach( iIndex, member; Descriptor.init.tupleof )
 		{
+			string[] strAllDecls;
+
+			foreach( uda; __traits( getAttributes, Descriptor.tupleof[ iIndex ] ) )
+			{
+				strUDAs ~= uda.stringof;
+			}
+
+			static if( IsUserType!( typeof( Descriptor.tupleof[ iIndex ] ) ) )
+			{
+				enum TypeString = ModuleLocalTypeName!( typeof( Descriptor.tupleof[ iIndex ] ) );
+			}
+			else
+			{
+				enum TypeString = typeof( Descriptor.tupleof[ iIndex ] ).stringof;
+			}
+
+			strAllDecls ~= TypeString ~ "[]";
+			strAllDecls ~= __traits( identifier, Descriptor.tupleof[ iIndex ] );
+
+			strOutput ~= strAllDecls.joinWith( " " );
 		}
 
-		return strOutput.joinWith( ";\n" );
+		return strOutput.joinWith( "\t", ";", "\n" );
 	}
 
-	return "";
+	string generateAllInternals()
+	{
+		return generateIterator()
+				~ "\n\npublic:\n"
+				~ generateVariables()
+				~ "\n\n"
+				~ generateLengthProperties()
+				~ "\n\n"
+				~ generateOperators();
+	}
+
+	//pragma( msg, generateAllInternals() );
+
+	return generateAllInternals();
 }
 //----------------------------------------------------------------------------
+
 
 //============================================================================
